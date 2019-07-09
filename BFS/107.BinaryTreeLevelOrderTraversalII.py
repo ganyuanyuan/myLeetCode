e638af51157f55d9d9bc3189ad7b2f8dcf6ae1bd
ganyuanyuan <ganyuanyuancc@gmail.com>
2019-07-09 19:36:34 -0400

    bfs

---
+++ BFS/107.BinaryTreeLevelOrderTraversalII.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        queue = [root]
+        ans = []
+        while queue:
+            nextQueue = []
+            res = []
+            for node in queue:
+                res.append(node.val)
+                if node.left:
+                    nextQueue.append(node.left)
+                if node.right :
+                    nextQueue.append(node.right)
+
+            ans.append(res)
+            queue = nextQueue
+        ans.reverse()
+        return ans
+
+
+#####################
+# no need to explain, just regular bfs.
