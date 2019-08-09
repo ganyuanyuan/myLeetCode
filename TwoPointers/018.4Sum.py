1cdc1d9f7234ff017ba7ca9cce3d86deca43fef7
ganyuanyuan <ganyuanyuancc@gmail.com>
2019-08-09 14:18:20 -0400

    hhhhh

---
+++ TwoPointers/018.4Sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if not nums or len(nums)<4:
+            return []
+        nums.sort()
+        res = []
+        n = len(nums)
+        for i in range(n-3):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1, n-2):
+                if j != i+1 and nums[j]==nums[j-1]:
+                    continue
+                sum = target - nums[i] - nums[j]
+                left, right = j+1, n-1
+                while left<right:
+                    if nums[left]+nums[right] == sum:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        left +=1
+                        right -=1
+                        while left<right and nums[left] == nums[left-1]:
+                            left +=1
+                    elif nums[left]+nums[right]< sum:
+                        left +=1
+                    else:
+                        right -=1
+        return res 
